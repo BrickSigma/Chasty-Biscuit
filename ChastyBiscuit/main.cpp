2305b18a02e75adda329a6c6f6fa57f28e90b969
Mohamed Junaid Chaudhry <bricksigma176@gmail.com>
2025-08-25 21:01:01 +0300

    Ported app to work on Windows with MSVC

--- ChastyBiscuit/main.cpp
+++ ChastyBiscuit/main.cpp
@@ -1,5 +1,11 @@
+#ifdef _MSC_VER
+#include <SDL.h>
+#include <SDL_ttf.h>
+#else
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#endif
+
 #include <stdbool.h>
 
 #include <Application.h>
@@ -13,13 +19,13 @@
 
 typedef struct AppCtx
 {
-    SDL_Event event;
-    SDL_Renderer *r;
+	SDL_Event event;
+	SDL_Renderer* r;
 
-    TTF_Font *font;
-    Text *text;
-    SDL_Rect text_pos;
-    char fps_buf[32];
+	TTF_Font* font;
+	Text* text;
+	SDL_Rect text_pos;
+	char fps_buf[32];
 } AppCtx;
 
 /* ================================================================ */
@@ -27,79 +33,77 @@ typedef struct AppCtx
 /**
  * Main game loop code
  */
-void game_loop(void *app_ctx)
+void game_loop(void* app_ctx)
 {
-    AppCtx *ctx = (AppCtx *)app_ctx;
+	AppCtx* ctx = (AppCtx*)app_ctx;
 
-    while (SDL_PollEvent(&(ctx->event)))
-    {
+	while (SDL_PollEvent(&(ctx->event)))
+	{
 
-        switch (ctx->event.type)
-        {
+		switch (ctx->event.type)
+		{
 
-        case SDL_QUIT:
-            App_stop();
-            break;
-        }
-    }
+		case SDL_QUIT:
+			App_stop();
+			break;
+		}
+	}
 
-    /* ================ */
+	/* ================ */
 
-    SDL_SetRenderDrawColor(ctx->r, 255, 0, 0, 255);
-    SDL_RenderClear(ctx->r);
+	SDL_SetRenderDrawColor(ctx->r, 255, 0, 0, 255);
+	SDL_RenderClear(ctx->r);
 
-    Text_draw(ctx->text, &ctx->text_pos);
+	Text_draw(ctx->text, &ctx->text_pos);
 
-    sprintf(ctx->fps_buf, "FPS: %d", get_fps());
-    Text_update(ctx->text, ctx->fps_buf);
+	sprintf(ctx->fps_buf, "FPS: %d", get_fps());
+	Text_update(ctx->text, ctx->fps_buf);
 
-    App_render();
+	App_render();
 
-    // printf("Delta time: %f\n", get_delta());
+	// printf("Delta time: %f\n", get_delta());
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    /* ================================ */
+	(void)argc;
+	(void)argv;
+	/* ================================ */
 
-    AppCtx ctx;
+	AppCtx ctx{};
 
-    const char *title = "Chasty Biscuit";
-    char buff[20];
-    strncpy(buff, title, 20);
-    ApplicationConfig app_config = {.title = buff,
-                                    .width = 640,
-                                    .height = 480,
-                                    .window_flags = SDL_WINDOW_SHOWN,
-                                    .renderer_flags = SDL_RENDERER_ACCELERATED};
+	const char* title = "Chasty Biscuit";
+	char buff[20];
+	strncpy(buff, title, 20);
+	ApplicationConfig app_config = { buff,640,480,SDL_WINDOW_SHOWN, SDL_RENDERER_ACCELERATED };
 
-    Start();
-    App_init(&app_config);
+	Start();
+	App_init(&app_config);
 
-    ctx.r = get_context();
+	ctx.r = get_context();
 
-    ctx.font = TTF_OpenFont("./resources/8bitOperatorPlus8-Regular.ttf", 24);
-    SDL_Color text_color = {0, 0, 0, 255};
-    ctx.text = Text_new(ctx.r, ctx.font, &text_color, "FPS: 60");
-    ctx.text_pos = (SDL_Rect){32, 32, ctx.text->width, ctx.text->height};
+	ctx.font = TTF_OpenFont("./resources/8bitOperatorPlus8-Regular.ttf", 24);
+	SDL_Color text_color = { 0, 0, 0, 255 };
+	ctx.text = Text_new(ctx.r, ctx.font, &text_color, "FPS: 60");
+	ctx.text_pos = SDL_Rect{ 32, 32, ctx.text->width, ctx.text->height };
 
-    App_setFPS(45);
+	App_setFPS(45);
 
 #ifdef __EMSCRIPTEN__
-    emscripten_set_main_loop_arg(game_loop, &ctx, 0, true);
+	emscripten_set_main_loop_arg(game_loop, &ctx, 0, true);
 #else
-    while (App_isRunning())
-        game_loop(&ctx);
+	while (App_isRunning())
+		game_loop(&ctx);
 #endif
 
-    /* ================================ */
+	/* ================================ */
 
-    TTF_CloseFont(ctx.font);
-    Text_destroy(&(ctx.text));
+	TTF_CloseFont(ctx.font);
+	Text_destroy(&(ctx.text));
 
-    App_quit();
-    Stop();
+	App_quit();
+	Stop();
 
-    /* ======== */
-    return 0;
+	/* ======== */
+	return 0;
 }
